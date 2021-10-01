02db08d6abf608da7f205603d14554da069c7547
Ankur Kedia <ankur.kedia@paytm.com>
2021-10-01 22:33:32 +0530

    Changed calculations to EAR

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ const loadModel = async () => {
   );
 };
 
-const thresholdValue = {
-  // NOTE: Values derived based on samples at different focal lengths. Need to verify this on different devices.
-  left: { angle: -0.986, y: 12.363 },
-  right: { angle: -1.0555, y: 10.3 },
-};
+const earThreshold = 0.23;
 
 const setUpCamera = async (videoElement, webcamId = undefined) => {
   video = videoElement;
@@ -59,40 +55,58 @@ function getThreshold(dir, irisZ) {
   return thresholdValue[dir]['angle'] * irisZ + thresholdValue[dir]['y'];
 }
 
+function getEucledianDistance(x1, y1, x2, y2) {
+  return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
+
+function getEAR(upper, lower) {
+  return (
+    (getEucledianDistance(upper[5][0], upper[5][1], lower[4][0], lower[4][1]) +
+      getEucledianDistance(
+        upper[3][0],
+        upper[3][1],
+        lower[2][0],
+        lower[2][1]
+      )) /
+    (2 *
+      getEucledianDistance(upper[0][0], upper[0][1], upper[8][0], upper[8][1]))
+  );
+}
+
 async function renderPrediction() {
   const predictions = await model.estimateFaces({
     input: video,
     returnTensors: false,
-    flipHorizontal: true,
-    predictIrises: true,
+    flipHorizontal: false,
+    predictIrises: false,
   });
 
-  // STEP: Detect a simple blink first
   if (predictions.length > 0) {
     predictions.forEach((prediction) => {
       // NOTE: Iris position did not work as the diff remains almost same, so trying 0th upper and lower eyes
       // NOTE: Error in docs, rightEyeLower0 is mapped to rightEyeUpper0 and vice-versa
       // NOTE: taking center point for now, can add more points for accuracy(mabye)
-      let rightLowerEyePoint = prediction.annotations.rightEyeUpper0[3];
-      let rightUpperEyePoint = prediction.annotations.rightEyeLower0[4];
 
-      let leftLowerEyePoint = prediction.annotations.leftEyeUpper0[3];
-      let leftUpperEyePoint = prediction.annotations.leftEyeLower0[4];
+      // Other logic
+      // NOTE: Found another way to detect it by Eye Aspect Ratio https://www.pyimagesearch.com/2017/04/24/eye-blink-detection-opencv-python-dlib/
+      // NOTE: Found it to be more accurate and gives better prection in cases where earlier method did not work.
+      let lowerRight = prediction.annotations.rightEyeUpper0;
+      let upperRight = prediction.annotations.rightEyeLower0;
+      const rightEAR = getEAR(upperRight, lowerRight);
 
-      let rightDy = rightUpperEyePoint[1] - rightLowerEyePoint[1];
-      let leftDy = leftUpperEyePoint[1] - leftLowerEyePoint[1];
+      let lowerLeft = prediction.annotations.leftEyeUpper0;
+      let upperLeft = prediction.annotations.leftEyeLower0;
+      const leftEAR = getEAR(upperLeft, lowerLeft);
 
-      let rightIrisZ = prediction.annotations.rightEyeIris[0][2];
-      let leftIrisZ = prediction.annotations.leftEyeIris[0][2];
-
-      let rightClosed = rightDy <= getThreshold('right', rightIrisZ);
-      let leftClosed = leftDy <= getThreshold('left', leftIrisZ);
-
+      // console.log('*** 🔥 Ear', leftEAR.toFixed(3), rightEAR.toFixed(3));
+      // if (leftEAR <= earThreshold || rightEAR <= earThreshold) {
+      //   console.log('*****************');
+      // }
       event = {
-        left: leftClosed,
-        right: rightClosed,
-        wink: leftClosed || rightClosed,
-        blink: leftClosed && rightClosed,
+        left: leftEAR <= earThreshold,
+        right: rightEAR <= earThreshold,
+        wink: leftEAR <= earThreshold || rightEAR <= earThreshold,
+        blink: leftEAR <= earThreshold && rightEAR <= earThreshold,
       };
     });
   }
